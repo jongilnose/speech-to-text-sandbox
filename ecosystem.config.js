1041786c6906e7a0038f0f98427b6642eaf155ca
Bobby <jongilnose@twosun.com>
2023-06-28 14:18:31 +0900

    Upt. env, ecosystem, package

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
   apps: [
     {
       name: 'speech-to-text-sandbox',
-      exec_mode: "cluster",
+      exec_mode: 'cluster',
       cwd: './',
       script: 'npm',
       args: 'start',
-      instances: "1",
+      instances: '1',
       autorestart: true,
       listen_timeout: 50000,
       kill_timeout: 5000,
-    }
-  ]
-}
+    },
+  ],
+};
